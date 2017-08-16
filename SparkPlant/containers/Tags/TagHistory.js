fef5713ddc2289d7b08869aa76bdd20bbbbf0bf4
Gaellan <mari.doucet@sooyoos.com>
2017-08-16 11:45:39 +0200

    adding loader to tag history

--- SparkPlant/containers/Tags/TagHistory.js
+++ SparkPlant/containers/Tags/TagHistory.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     View,
+    ActivityIndicator,
 } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -51,19 +52,39 @@ class TagHistory extends Component {
 
     render() {
         let tag = this.props.tags.currentTag;
-        return (
-            <View style={styles.login}>
-                <HeaderTagDetails {...this.props} headerTitle={"#" + lpad(tag["@id"].substr(tag["@id"].lastIndexOf("/") +1), 6)} />
-                <View style={styles.list}>
-                    <TagHistoryList {...this.props} items={tag.history} />
+
+        if(this.props.tags.loading === true)
+        {
+            return (
+                <View style={styles.login}>
+                    <HeaderTagDetails {...this.props} headerTitle={"#" + lpad(tag["@id"].substr(tag["@id"].lastIndexOf("/") +1), 6)} />
+                    <View style={styles.list}>
+                        <ActivityIndicator color="#3f51b5" size="large"/>
+                    </View>
+                    <View style={styles.footer}>
+                        <FooterButton {...this.props} active={false} tag={tag} iconName="sticky-note-o" text="Contenu" route={this.props.goToTagDetails}/>
+                        <FooterButton {...this.props} active={true} tag={tag} iconName="info" text="Historique" route={this.props.goToTagHistory}/>
+                        <FooterButton {...this.props} active={false} tag={tag} iconName="exchange" text="Actions" route={this.props.goToTagAction}/>
+                    </View>
                 </View>
-                <View style={styles.footer}>
-                    <FooterButton {...this.props} active={false} tag={tag} iconName="sticky-note-o" text="Contenu" route={this.props.goToTagDetails}/>
-                    <FooterButton {...this.props} active={true} tag={tag} iconName="info" text="Historique" route={this.props.goToTagHistory}/>
-                    <FooterButton {...this.props} active={false} tag={tag} iconName="exchange" text="Actions" route={this.props.goToTagAction}/>
+            );
+        }
+        else
+        {
+            return (
+                <View style={styles.login}>
+                    <HeaderTagDetails {...this.props} headerTitle={"#" + lpad(tag["@id"].substr(tag["@id"].lastIndexOf("/") +1), 6)} />
+                    <View style={styles.list}>
+                        <TagHistoryList {...this.props} items={tag.history} />
+                    </View>
+                    <View style={styles.footer}>
+                        <FooterButton {...this.props} active={false} tag={tag} iconName="sticky-note-o" text="Contenu" route={this.props.goToTagDetails}/>
+                        <FooterButton {...this.props} active={true} tag={tag} iconName="info" text="Historique" route={this.props.goToTagHistory}/>
+                        <FooterButton {...this.props} active={false} tag={tag} iconName="exchange" text="Actions" route={this.props.goToTagAction}/>
+                    </View>
                 </View>
-            </View>
-        );
+            );
+        }
     }
 };
 
