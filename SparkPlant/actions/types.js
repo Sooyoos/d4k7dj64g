70ac9611bf4a584edb97a48d90b50533eaec336d
Gaellan <mari.doucet@sooyoos.com>
2018-02-01 17:17:14 +0100

    making the app use the testing api (not the develop)

--- SparkPlant/actions/types.js
+++ SparkPlant/actions/types.js
@@ -257,8 +257,8 @@ export const UNSOLVED_TAGS_REQUESTED = "UNSOLVED_TAGS_REQUESTED";
 export const UNSOLVED_TAGS_SUCCESS = "UNSOLVED_TAGS_SUCCESS";
 export const UNSOLVED_TAGS_ERROR = "UNSOLVED_TAGS_ERROR";
 
-//export const baseUrl = "https://sparkplant-api-testing.sooyoos.com";
-export const baseUrl = "http://sparkplant-api-develop.sooyoos.com";
+export const baseUrl = "https://sparkplant-api-testing.sooyoos.com";
+//export const baseUrl = "http://sparkplant-api-develop.sooyoos.com";
 
 export const RESET_LOGIN_ERROR = "RESET_LOGIN_ERROR";
 
