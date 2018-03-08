757ded6430dcd9a767f4504c1219cc6c38435704
Gaellan <mari.doucet@sooyoos.com>
2018-03-08 14:40:56 +0100

    taking out the fullscreen button for videos on tag creation

--- SparkPlant/components/Tags/TagMedia.js
+++ SparkPlant/components/Tags/TagMedia.js
@@ -69,24 +69,44 @@ class TagMedia extends Component {
         console.log(this.props.media);
         if(this.props.active === true)
         {
-            return(
-                <ElevatedView style={styles.mediaCard} elevation={2}>
-                    <TouchableWithoutFeedback onPress={() => { this.props.deactivateItem() }}>
-                        <View style={styles.buttonView}>
-                            <TouchableOpacity onPress={() => { this.props.removeItem(this.props.index) }}>
-                                <ElevatedView style={styles.deleteButton} elevation={2}>
-                                    <Icon name="trash-o" style={styles.buttonIcon} />
-                                </ElevatedView>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={() => { this.props.expandItem(this.props.media) }}>
-                                <ElevatedView style={styles.fullscreenButton} elevation={2}>
-                                    <Icon name="expand" style={styles.buttonIcon} />
-                                </ElevatedView>
-                            </TouchableOpacity>
-                        </View>
-                    </TouchableWithoutFeedback>
-                </ElevatedView>
-            );
+            if(this.props.media.type && this.props.media.type.indexOf("video") === -1) // image
+            {
+                return(
+                    <ElevatedView style={styles.mediaCard} elevation={2}>
+                        <TouchableWithoutFeedback onPress={() => { this.props.deactivateItem() }}>
+                            <View style={styles.buttonView}>
+                                <TouchableOpacity onPress={() => { this.props.removeItem(this.props.index) }}>
+                                    <ElevatedView style={styles.deleteButton} elevation={2}>
+                                        <Icon name="trash-o" style={styles.buttonIcon} />
+                                    </ElevatedView>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => { this.props.expandItem(this.props.media) }}>
+                                    <ElevatedView style={styles.fullscreenButton} elevation={2}>
+                                        <Icon name="expand" style={styles.buttonIcon} />
+                                    </ElevatedView>
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </ElevatedView>
+                );
+            }
+            else
+            {
+                return(
+                    <ElevatedView style={styles.mediaCard} elevation={2}>
+                        <TouchableWithoutFeedback onPress={() => { this.props.deactivateItem() }}>
+                            <View style={styles.buttonView}>
+                                <TouchableOpacity onPress={() => { this.props.removeItem(this.props.index) }}>
+                                    <ElevatedView style={styles.deleteButton} elevation={2}>
+                                        <Icon name="trash-o" style={styles.buttonIcon} />
+                                    </ElevatedView>
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </ElevatedView>
+                );
+            }
+
         }
         else
         {
